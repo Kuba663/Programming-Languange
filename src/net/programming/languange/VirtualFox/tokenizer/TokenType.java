8fd3f45068aeb764b219259ff7b187d2d74ee1e9
Kuba663 <kuba63b@gmail.com>
2016-10-08 13:32:25 +0200

    TokenType

---
+++ src/net/programming/languange/VirtualFox/tokenizer/TokenType.java
@@ -0,0 +1,12 @@
+package net.programming.languange.VirtualFox.tokenizer;
+
+public enum TokenType {
+	TOKEN,
+	IDENTIFER,
+	INTEGER_LITERAL,
+	STRING_LITERAL,
+	FLOAT_LITERAL,
+	REGISTER_LITERAL,
+	BYTE_LITERAL,
+	EMPTY
+}
