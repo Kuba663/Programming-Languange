e09b072aae0072eb1b296f2cb44ccbf16c055fc3
Kuba663 <kuba63b@gmail.com>
2016-10-08 13:43:03 +0200

    Class

---
+++ src/net/programming/languange/VirtualFox/block/Class.java
@@ -0,0 +1,23 @@
+package net.programming.languange.VirtualFox.block;
+
+public class Class extends Block {
+	
+	private String name;
+	
+	public Class(String name) {
+		super(null);
+		
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
